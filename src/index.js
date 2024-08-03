933f6d69bea5edc3f3e64d809a5657ae0122d854
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2024-08-03 16:26:14 -0700

    learn config router and controller with expressjs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const handlebars = require('express-handlebars');
+const route = require('./routes/index.route');
 
 const app = express();
 const port = 3000;
@@ -21,23 +22,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  console.log(req.query.q);
-  res.render('news');
-})
-
-app.get('/search', (req, res) => {
-  res.render('search');
-})
-
-app.post('/search', (req, res) => {
-  console.log(req.body);
-  res.send('');
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ src/routes/index.route.js
@@ -0,0 +1,14 @@
+const newsRouter = require('./news.route');
+const sitesRouter = require('./site.route');
+
+function route(app) {
+    app.use('/news', newsRouter);
+    app.use('/', sitesRouter);
+
+    // app.post('/search', (req, res) => {
+    //     console.log(req.body);
+    //     res.send('');
+    // })
+}
+
+module.exports = route;
