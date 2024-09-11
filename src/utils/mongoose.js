3902566ad744006064e9c08d1a955233249c2bb5
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2024-09-11 13:43:21 -0700

    learn about nodejs on youtube

--- src/utils/mongoose.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  multipleMongooseToObject: (mongooses) =>
-    mongooses.map((item) => item.toObject()),
-
-  mongooseToObject: (mongoose) => (mongoose ? mongoose.toObject() : mongoose),
-};
